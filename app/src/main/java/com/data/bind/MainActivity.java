a066fb2713b9474978c6bf15440fadf870e440b9
ming <cy_wangming@163.com>
2017-09-05 17:35:59 +0800

    Initial commit

---
+++ app/src/main/java/com/data/bind/MainActivity.java
@@ -0,0 +1,34 @@
+package com.data.bind;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+
+import com.data.bind.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private GridLayoutManager manager;
+    private MainAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        adapter = new MainAdapter(this);
+        manager = new GridLayoutManager(this, LinearLayoutManager.VERTICAL);
+        binding.recyclerView.setLayoutManager(manager);
+        binding.recyclerView.setAdapter(adapter);
+        List<User> list = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            User user = new User("jack" + i, i);
+            list.add(user);
+        }
+        adapter.setUserList(list);
+
+    }
+}
---
+++ app/src/main/java/com/data/bind/MainAdapter.java
@@ -0,0 +1,65 @@
+package com.data.bind;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.data.bind.databinding.ItemBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by wangmingming on 2017/9/5.
+ */
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+    private LayoutInflater inflater;
+    private Context mContext;
+    private List<User> userList = new ArrayList<>();
+
+
+    public MainAdapter(Context context) {
+        mContext = context;
+        inflater = LayoutInflater.from(context);
+    }
+
+    public void setUserList(List<User> list) {
+        userList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        ItemBinding binding = ItemBinding.inflate(inflater, parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.binding.setUser(userList.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ItemBinding binding = null;
+
+        public ViewHolder(ItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bindData(User user) {
+            binding.setUser(user);
+            binding.executePendingBindings();
+        }
+    }
+}
